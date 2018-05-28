f85e6a87175003363feb74371652e76a659bf92f
radevon <radevon@mail.ru>
2018-05-28 15:37:29 +0300

    Добавил вывод температуры из сервиса

--- MonoIndication/MonoIndication/js/programm.js
+++ MonoIndication/MonoIndication/js/programm.js
@@ -53,9 +53,61 @@ function trackEvents(url, seconds_ago) {
     });
 }
 
+function openweathermap(fn) {
+    
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=Chojniki&appid=f62c1e09b0f0ac4468daf91c3fd39089&units=metric';
+        $.getJSON(url + '&callback=?', {}, function (data) {
+            
+            fn(data);
+        });
+   
+}
+
 function customAlert(message, clas,delay) {
     var aler = $("<div class='alert fixedAlert " + clas + " alert-dismissable'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>×</button>" + message + "</div>");
     $("body").append(aler);
     $(aler).delay(delay*1000).fadeOut("slow", function () { $(this).remove(); });
 
+}
+
+var RedBlueColorGenerator = function (minValue, maxValue) {
+
+    if (minValue === undefined || maxValue === undefined)
+        throw "Диаппазон изменения переменной не задан!";
+
+    if (minValue === maxValue)
+        throw "Диаппазон значений заданн не корректно. Границы равны!";
+
+    if(+minValue>+maxValue){
+        this.minValue = +maxValue;
+        this.maxValue = +minValue;
+    }else
+    {
+        this.minValue = +minValue;
+        this.maxValue = +maxValue;
+    }
+    
+
+    this.delta = 255 / (this.maxValue - this.minValue);
+
+    this.R = function (T) {
+        return this.delta * (T - this.minValue);
+    }
+
+    this.G = function (T) {
+        return 130;
+    }
+
+    this.B = function (T) {
+        return this.delta * (this.maxValue-T);
+    }
+
+    this.RGB = function (T) {
+        if (T < this.minValue)
+            T=this.minValue;
+        if (T > this.maxValue)
+            T = this.maxValue;
+            
+        return 'rgb('+this.R(T) + ',' + this.G(T) + ',' + this.B(T)+')';
+    }
 }
